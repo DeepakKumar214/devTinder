155067a08525be751549dbbf7b18a469f052d46c
DeepakKumar214 <deepakkumar.dubey@necsws.com>
2024-11-17 20:13:02 +0530

    exploring GET, POST ,DELETE methods of express

--- src/app.js
+++ src/app.js
@@ -2,16 +2,25 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({
+    userName: "Deepak",
+    lastname: "dubey",
+  });
+});
+
+app.post("/user", (req, res) => {
+  //logic to save user details in to database
+  res.send("User details haas been saved into DB ");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("user data deleted");
+});
+
+//this will match all HTTP methods calls
 app.use("/test", (req, res) => {
   res.send("Hello from server:)");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("hello hello heloo");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello fron dev tinder ");
 });
 
 app.listen(3000, () => {
